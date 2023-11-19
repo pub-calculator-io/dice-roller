d3aa1a24a80aa6000843a51c8fe1762d31c40797
Arseniy Olevskiy <arsidian@gmail.com>
2023-11-19 13:26:39 +0400

    calculator changes commit

--- plugin.php
+++ plugin.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: Dice Roller by Calculator.iO
+Plugin Name: Dice Roller by www.calculator.io
 Plugin URI: https://www.calculator.io/dice-roller/
 Description: This online dice roller uses random number generation to create a digital dice rolling experience with all sorts of beneficial uses.
 Version: 1.0.0
@@ -16,7 +16,7 @@ if (!function_exists('add_shortcode')) return "No direct call for Dice Roller by
 
 function display_ci_dice_roller(){
     $page = 'index.html';
-    return '<h2><a href="https://www.calculator.io/dice-roller/" target="_blank"><img src="' . esc_url(plugins_url('assets/images/icon-48.png', __FILE__ )) . '" width="48" height="48"></a> Dice Roller</h2><div><iframe style="background:transparent; overflow: scroll" src="' . esc_url(plugins_url($page, __FILE__ )) . '" width="100%" frameBorder="0" allowtransparency="true" onload="this.style.height = this.contentWindow.document.documentElement.scrollHeight + \'px\';" id="ci_dice_roller_iframe"></iframe></div>';
+    return '<h2><img src="' . esc_url(plugins_url('assets/images/icon-48.png', __FILE__ )) . '" width="48" height="48">Dice Roller</h2><div><iframe style="background:transparent; overflow: scroll" src="' . esc_url(plugins_url($page, __FILE__ )) . '" width="100%" frameBorder="0" allowtransparency="true" onload="this.style.height = this.contentWindow.document.documentElement.scrollHeight + \'px\';" id="ci_dice_roller_iframe"></iframe></div>';
 }
 
 add_shortcode( 'ci_dice_roller', 'display_ci_dice_roller' );
